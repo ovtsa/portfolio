db40a079295dc51cff7ce5a7b5abf1f2cf13634c
Nathan Jobe <36213662+ovtsa@users.noreply.github.com>
2022-06-18 23:23:24 -0400

    Added resume button to contact page

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactMeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  onDownloadResumeButtonClicked() {
+    const path = '/assets/misc/dummy_resume.docx';
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', window.location.protocol + '//' + window.location.host + path);
+    link.setAttribute('download', `dummy_resume.docx`);
+    link.click();
+    link.remove();
+  }
 }
