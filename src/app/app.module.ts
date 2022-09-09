5b79a14a1ab1249a60066fccc4df94905eac86bc
Nathan Jobe <36213662+ovtsa@users.noreply.github.com>
2022-09-09 16:27:26 -0500

    added sticky navbar css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
+import { NavbarButtonComponent } from './projects-navbar/navbar-button/navbar-button.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AuthGuard } from './auth-guard.service';
     HeaderButtonComponent,
     ContactMeComponent,
     SupportComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    NavbarButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/projects-navbar/navbar-button/navbar-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-button',
+  templateUrl: './navbar-button.component.html',
+  styleUrls: ['./navbar-button.component.css']
+})
+export class NavbarButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
